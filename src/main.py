d939ca2fea55986b3a9df75efe1a5e46fad939e3
TheDomesticUser <TheDomesticUser@gmail.com>
2020-04-08 21:42:57 -0400

    Added signup functionality

--- src/main.py
+++ src/main.py
@@ -4,17 +4,48 @@ kivy.require('1.11.1')
 
 from kivy.app import App
 from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.properties import ObjectProperty
 
 import sqlite3
+from argon2 import PasswordHasher
 
-class AuthDisplay(AnchorLayout):
+class UserSignUp(BoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        
+        self.conn = sqlite3.connect('users.db')
+        self.cursor = self.conn.cursor()
+
+    def user_sign_up(self, username, password):
+        table_name = 'users'
+        ph = PasswordHasher()
+
+        hashed_password = ph.hash(password)
+
+        # save the user credentials
+        self.cursor.execute('''
+            INSERT INTO %s(username, password)
+            VALUES('%s', '%s')
+        ''' % (table_name, username, hashed_password))
+
+        self.conn.commit()
+
+class UserLogin(GridLayout):
+    pass
+
+class Auth(AnchorLayout):
     pass
 
 class AuthApp(App):
     def build(self):
-        authDisplay = AuthDisplay()
+        auth = Auth()
 
-        return authDisplay
+        return auth
+
+    def on_stop():
+        print('closed connection')
 
 if __name__ == '__main__':
     AuthApp().run()
